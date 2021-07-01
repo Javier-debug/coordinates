6c933e4a88e396eb71fa067ef653382917e882bd
Javier-debug <madrajavi@gmail.com>
2021-07-01 13:05:10 -0500

    Login

--- js/index.js
+++ js/index.js
@@ -42,6 +42,24 @@ btnIngresar.addEventListener("click", () => {
   });
 })
 
+function mensajeError(codigo) {
+  let mensaje = '';
+  switch(codigo) {
+    case 'auth/wrong-password': 
+    mensaje = "Contraseña incorrecta"
+    break;
+    case 'auth/user-not-found': 
+    mensaje = "Usuario no encontrado"
+    break;
+    case 'auth/weak-password': 
+    mensaje = "Contraseña debil"
+    break;
+    default: 
+    mensaje = "Occurio un error al ingresar con este usuario"
+  }
+  return mensaje;
+}
+
 /*
 input.addEventListener("change", function() {
   dataArray = [];
